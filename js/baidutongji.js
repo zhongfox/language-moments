259a5f3d5a454995d5d6dc043b05a1b4e9e2be6b
zhonghua <zhonghua@cd.tuan800.com>
2017-02-12 02:33:08 +0800

    增加百度统计

---
+++ js/baidutongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?bfef5c80d6d47982aba064a8fe15ad72";
+        var s = document.getElementsByTagName("script")[0]; 
+          s.parentNode.insertBefore(hm, s);
+})();
